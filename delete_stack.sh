4b69c0c1cf15c645ede5c60abc526c98552ca04f
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2016-02-09 14:11:06 +0000

    Interim

--- delete_stack.sh
+++ delete_stack.sh
@@ -14,35 +14,42 @@ runaws() {
 }
 
 declare AppName
+
 read -p "Enter App/StackName: " AppName
 LBName="lb-${AppName}"
 SecurityGroupName="secgroup-${AppName}"
 SubnetName="subnet-${AppName}"
 
+echo "Getting instances"
 InstanceIDs=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" "Name=tag:Name,Values=${AppName}" | grep INSTANCES | cut -f8)
-SecurityGroupID=$(aws ec2 describe-security-groups --filters "Name=tag:Name,Values=${SecurityGroupName}" | grep SECURITYGROUPS | cut -f3)
-SubnetID=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=${SubnetName}" | grep SUBNETS | cut -f8)
-
-
-echo "Instances ${InstanceIDs}"
-echo "SecurityG ${SecurityGroupID}"
-echo "Subnet ${SubnetID}"
-
+echo "Instances: ${InstanceIDs}"
 if [[ "${InstanceIDs}" != "" ]]; then
   delay=5
-  PendingIDs=
+  TerminatedIDs=
+  echo "deregistering instances from loadbalancers"
+  aws elb deregister-instances-from-load-balancer --load-balancer-name ${LBName} --instances ${InstanceIDs}
   echo "terminating instances"
   aws ec2 terminate-instances --instance-ids ${InstanceIDs}
-  while [[ ! "${InstanceIDs}" == "" && -z "${PendingIDs}" ]]; do
+  while [[ ! "${InstanceIDs}" == "" && -z "${TerminatedIDs}" ]]; do
     echo "Waiting ${delay}s for instances to terminate..."
     sleep ${delay}
-    PendingIDs=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=terminated" "Name=tag:Name,Values=${AppName}" | grep INSTANCES | cut -f8)
+    TerminatedIDs=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=terminated" "Name=tag:Name,Values=${AppName}" | grep INSTANCES | cut -f8)
   done
 fi
 
-echo "deleting load balancer"
+echo "Deleting load balancer"
 aws elb delete-load-balancer --load-balancer-name ${LBName}
-echo "deleting security groups"
+
+echo "Getting security group"
+SecurityGroupID=$(aws ec2 describe-security-groups --filters "Name=tag:Name,Values=${SecurityGroupName}" | grep SECURITYGROUPS | cut -f3)
+echo "Security group: ${SecurityGroupID}"
+
+echo "Getting subnet"
+SubnetID=$(aws ec2 describe-subnets --filters "Name=tag:Name,Values=${SubnetName}" | grep SUBNETS | cut -f8)
+echo "Subnet: ${SubnetID}"
+
+echo "Deleting security groups"
 aws ec2 delete-security-group --group-id ${SecurityGroupID}
-echo "deleting subnet"
+
+echo "Deleting subnet"
 aws ec2 delete-subnet --subnet-id ${SubnetID}
