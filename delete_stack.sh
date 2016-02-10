d800d752d757cdf2112e6199d9097e65cc59d256
Ayub Malik <ayub.malik@dwp.gsi.gov.uk>
2016-02-10 13:06:55 +0000

    Delete stack removed errors

--- delete_stack.sh
+++ delete_stack.sh
@@ -13,9 +13,9 @@ runaws() {
   #awsresult=$(aws ${cmd})
 }
 
-
-declare AppName
 declare Delay
+declare AppName
+
 read -p "Enter App/StackName: " AppName
 LBName="lb-${AppName}"
 SecurityGroupName="secgroup-${AppName}"
@@ -26,32 +26,27 @@ InstanceIDs=$(aws ec2 describe-instances --filters "Name=instance-state-name,Val
 echo "Instances: ${InstanceIDs}"
 if [[ ! -z ${InstanceIDs} ]]; then
   TerminatedIDs=
-  echo "deregistering instances from loadbalancers"
+  echo "Deregistering instances from loadbalancers"
   aws elb deregister-instances-from-load-balancer --load-balancer-name ${LBName} --instances ${InstanceIDs}
-  echo "terminating instances"
-  aws ec2 terminate-instances --instance-ids ${InstanceIDs}
+  echo "Terminating instances"
+  result=$(aws ec2 terminate-instances --instance-ids ${InstanceIDs})
   while [[ ${TerminatedIDs} != ${InstanceIDs} ]]; do
     echo "Waiting ${Delay}s for all instances to terminate. Currently terminated: ${TerminatedIDs}"
     sleep ${Delay}
     TerminatedIDs=$(aws ec2 describe-instances --filters "Name=instance-state-name,Values=terminated" "Name=tag:Name,Values=${AppName}" | grep INSTANCES | cut -f8)
   done
 fi
-echo "Checking instance state after termination"
-InstanceStates=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${AppName}" | grep 'STATE\t')
-echo "Instance states: ${InstanceStates}"
 
 echo "Deleting load balancer"
 aws elb delete-load-balancer --load-balancer-name ${LBName}
 
-NetworkInterfaceIDs=notempty
+NetworkInterfaceIDs=' '
 while [[ ! -z ${NetworkInterfaceIDs} ]]; do
-  echo "Waiting ${Delay}s for LB network interfaces to be released"
+  echo "Waiting ${Delay}s for LB network interfaces to be released: ${NetworkInterfaceIDs}"
   sleep ${Delay}
   NetworkInterfaceIDs=$(aws ec2 describe-network-interfaces --filters "Name=description,Values=ELB ${LBName}" | grep NETWORKINTERFACES | cut -f5)
-  echo "Network interfaces: ${NetworkInterfaceIDs}"
 done
 
-exit 0
 echo "Getting security group"
 SecurityGroupID=$(aws ec2 describe-security-groups --filters "Name=tag:Name,Values=${SecurityGroupName}" | grep SECURITYGROUPS | cut -f3)
 echo "Security group: ${SecurityGroupID}"
